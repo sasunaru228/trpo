8601a65ef440599c4fda82fba8c937d73f0bfb88
sasunaru228 <od26102000@gmail.com>
2020-04-21 14:42:44 +0300

    v2.0

--- index.php
+++ index.php
@@ -1,28 +1,56 @@
 <?php
-class A {
-
+class orlovException extends RuntimeException {
 }
 
-class B extends A {
-	function __construct($a, $b) {
-		$this->a = $a;
-		$this->b = $b;
-	}
-
+class Linear {
 	protected $a;
 	protected $b;
-}
-
-class C extends B {
-	function __construct($a, $b, $c) {
-		parent::__construct($a, $b);
-
-		$this->c = $c;
+	protected $x;
+	function __construct($a, $b){
+		$this->a=$a;
+		$this->b=$b;
 	}
-
-	protected $c;
+	function ur($a, $b){
+		if ($a != 0) {
+			$x = -1*$b/$a;
+			$this->x = $x;
+			return $x;
+		}
+		throw new orlovException("Нет корней");
+	}
 }
 
-$a = new C(new A, new A, new B(new A, new A));
-var_dump($a);
+class Square extends Linear {
+	protected $c;
+	protected $x2;
+	function __construct($a, $b, $c){
+		parent::__construct($a, $b);
+		$this->c=$c;
+	}
+	protected function bad($a, $b, $c) {
+		$bad = $b*$b - 4*$a*$c;
+		return $bad;
+	}
+	function ur2($a, $b, $c) {
+		$bad = $this->bad($a, $b, $c);
+		if ($a == 0){
+			$this ->ur($a , $b);
+		}
+		if ($bad > 0) {
+			$x = (-1*$b + sqrt($bad))/(2*$a);
+			$x2 = (-1*$b - sqrt($bad))/(2*$a);
+			$this->x = $x;
+			$this->x2 = $x2;
+			return array($x, $x2);
+		} elseif ($bad = 0) {
+			$x = (-1*$b)/(2*$a);
+			$this->x = $x;
+			return array($x);
+		}
+		throw new orlovException("Нет корней");
+	}
+}
+
+$solver = new Square(1, 2, 3);
+var_dump($solver->ur2(1, 2, -3));
 ?>
